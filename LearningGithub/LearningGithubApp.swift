7e1faab09e66e401be9754c74f8fb7d3a0f93953
Sainath <sainathbame1630@gmail.com>
2025-07-05 11:05:30 +0530

    first commit

---
+++ LearningGithub/LearningGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGithubApp.swift
+//  LearningGithub
+//
+//  Created by Sainath bamen on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ LearningGithub/LoginView.swift
@@ -0,0 +1,31 @@
+//
+//  LoginView.swift
+//  LearningGithub
+//
+//  Created by Sainath bamen on 05/07/25.
+//
+
+import SwiftUI
+
+
+struct LoginView: View {
+    @State var textField1 = ""
+    @State var textField2 = ""
+
+    var body: some View {
+        VStack {
+            
+            TextField("Name", text: $textField1)
+            TextField("Email", text: $textField2)
+
+        }
+        .padding()
+    }
+}
+
+
+
+
+#Preview {
+    LoginView()
+}
